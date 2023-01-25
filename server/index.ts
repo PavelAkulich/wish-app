77a2c2d6227868119f058aca65c3db15637c4559
Akulich Pavel <akulichpa@solit.by>
2023-01-25 17:31:34 +0300

    feat: front init

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (res, req) => req.status(200).json({
-  message: 'work lol'
-}));
+app.get('/', (res, req) => req.status(200).send('server work'));
 
 app.listen(5555, () => {
   console.log('work!!!');
